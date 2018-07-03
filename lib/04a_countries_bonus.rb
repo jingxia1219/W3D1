e62735f718c6b2a9bf2b1a1759979667e0d68353
z-nr <zayneyre@gmail.com>
2018-07-02 17:31:23 -0700

    Complete 05, 06, partial 07

--- lib/04a_countries_bonus.rb
+++ lib/04a_countries_bonus.rb
@@ -17,6 +17,7 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  
   SQL
 end
 
